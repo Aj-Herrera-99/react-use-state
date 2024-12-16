a573086899556a1afc5fad1f4517881a5ffaf933
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-12-16 16:51:20 +0100

    macrostruttura

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import languages from "./data/languages";
+
+const langs = [...languages];
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <>
+            {/* Header */}
+            <header className="p-4 bg-red-300">
+                <h1 className="text-5xl font-semibold">
+                    Learn Web Development
+                </h1>
+            </header>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            {/* Main */}
+            <main className="p-16 bg-green-300 ">
+                {/* LanguagesWrapper */}
+                <div className="flex flex-col gap-10 max-w-[700px] mx-auto">
+                    <div className="flex gap-4">
+                        {/* Language */}
+                        {langs.map((lang) => (
+                            <button className="px-4 py-2 text-white bg-blue-600 rounded-md">
+                                {lang.title}
+                            </button>
+                        ))}
+                    </div>
+                    {/* Description */}
+                    <div className="p-4 border-2 border-gray-400 rounded-md">
+                        <h4 className="text-xl font-semibold">
+                            {langs[0].title}
+                        </h4>
+                        <p>{langs[0].description}</p>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 }
 
-export default App
+export default App;
